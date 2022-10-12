20a3fee39681325575cafa065f7cedcdbc3190e6
leticia <leticia.rodrigues@ccc.ufcg.edu.br>
2022-10-12 14:54:11 -0300

    created test for product removal - 0% working

--- src/test/ProdutoTest.java
+++ src/test/ProdutoTest.java
@@ -12,15 +12,22 @@ class ProdutoTest {
 	private Facade mercadoFacade = new Facade(); 
 	
 	private String jsonP1 = "{\"nome\":\"Leite integral\", \"fabricante\":\"Parmalat\", \"preco\":10.5}";
+	private String idP1;
 
 	@BeforeEach
 	public void createProduto() {
-		mercadoFacade.criaProduto(jsonP1);
+		this.idP1 = mercadoFacade.criaProduto(jsonP1);
 	}
 	
 	@Test
-	public void verifyProdutosIgualUm() {
+	public void verifyProdutoAdicionado() {
 		assertEquals(1, mercadoFacade.listaProdutos().size());
+	}
+
+	@Test
+	public void verifyProdutoDeletado() {
+		mercadoFacade.removeProduto(idP1);
+		assertEquals(0, mercadoFacade.listaProdutos().size());
 	}
 
 }
