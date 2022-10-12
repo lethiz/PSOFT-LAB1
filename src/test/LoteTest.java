cf8c8e10856049e4dcefffa7faf776e521a6ca8c
leticia <leticia.rodrigues@ccc.ufcg.edu.br>
2022-10-12 20:38:34 -0300

    created test for lote removal when product is removed - 0% working

--- src/test/LoteTest.java
+++ src/test/LoteTest.java
@@ -12,20 +12,25 @@ class LoteTest {
     private Facade mercadoFacade = new Facade();
 
     private String jsonP1 = "{\"nome\":\"Leite integral\", \"fabricante\":\"Parmalat\", \"preco\":10.5}";
-
     private String idP1;
-
-    String jsonL1 = "{\"idProduto\":\"" + idP1 + "\", \"quantidade\":10}";
     private String idL1;
 
     @BeforeEach
     public void createLote() {
         this.idP1 = mercadoFacade.criaProduto(jsonP1);
+        String jsonL1 = "{\"idProduto\":\"" + idP1 + "\", \"quantidade\":10}";
         this.idL1 = mercadoFacade.criaLote(jsonL1);
     }
 
     @Test
     public void verifyLoteAdicionado() {
         assertEquals(1, mercadoFacade.listaLotes().size());
+    }
+
+    @Test
+    public void verifyLoteDeletado() {
+        assertEquals(1, mercadoFacade.listaLotes().size());
+        mercadoFacade.removeProduto(idP1);
+        assertEquals(0, mercadoFacade.listaLotes().size());
     }
 }
